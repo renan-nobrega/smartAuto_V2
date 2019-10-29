1beae1887058f48ace71791aa7a7a71f430ffb04
renan-nobrega <renan.nobrega@outlook.com>
2019-10-29 17:31:01 -0300

    Deixando o código em PHP e criando formularios

---
+++ Telas-Renan-Nao-mexer/SmartAutoV2/layout/head.php
@@ -0,0 +1,38 @@
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Home || SysAuto Escola</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+<!-- Fonts -->
+<link href="fonts/lato/lato.css" rel="stylesheet">
+<!-- CSS -->
+<!-- Bootstrap CSS
+============================================ -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<!-- Icon Font CSS
+============================================ -->
+<link rel="stylesheet" href="css/icofont.css">
+<!-- Plugins CSS
+============================================ -->
+<link rel="stylesheet" href="css/plugins.css">
+<!-- ShortCodes CSS
+============================================ -->
+<link rel="stylesheet" href="css/shortcode/shortcodes.css">
+<!-- Style CSS
+============================================ -->
+<link rel="stylesheet" href="style.css">
+<!-- Responsive CSS
+============================================ -->
+<link rel="stylesheet" href="css/responsive.css">
+<!-- Color Style -->
+<link href="css/color/color-1.css" rel="stylesheet">
+
+
+
+
+
+<!-- Modernizer JS
+============================================ -->
+<script src="js/vendor/modernizr-2.8.3.min.js"></script>
---
+++ Telas-Renan-Nao-mexer/SmartAutoV2/layout/header.php
@@ -0,0 +1,93 @@
+
+<!-- Pre Loader
+============================================ -->
+<div class="preloader">
+    <div class="loading-center">
+        <div class="loading-center-absolute">
+            <div class="object object_one"></div>
+            <div class="object object_two"></div>
+            <div class="object object_three"></div>
+        </div>
+    </div>
+</div>
+<!-- Body main wrapper start -->
+<div class="wrapper box-layout fix">
+    <!-- Header 1
+============================================ -->
+    <div class="header-area header-absolute header-transparent">
+        <div class="header-top hidden-xs">
+            <div class="container">
+                <div class="row">
+                    <!-- Header Top -->
+                    <div class="header-top-wrapper fix">
+                        <div class="header-top-left text-left col-sm-6">
+                            <p><i class="icofont icofont-envelope"></i><span>info@smartauto.com</span></p>
+                            <p><i class="icofont icofont-ui-call"></i><span>+55 99999-5999</span></p>
+                        </div>
+                        <div class="header-top-right text-right col-sm-6">
+                            <p><i class="icofont icofont-clock-time"></i><span>Mon - Sat : 8am - 9pm</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-bottom sticky">
+            <div class="container">
+                <div class="row">
+                    <!-- Header Bottom -->
+                    <div class="col-xs-12">
+                        <div class="navbar-header">
+                            <a href="index.html" class="logo navbar-brand"><img id="logo_img" src="img/logo/color-1.png" alt="logo" /></a>
+                        </div>
+                        <div class="main-menu mean-menu float-right">
+                            <nav>
+                                <ul>
+                                    <li class="active"><a href="index.html">home</a></li>
+                                    <!--
+
+<li><a href="gallery.html">gallery<i class="icofont icofont-simple-down"></i></a></li>
+-->
+                                    <li><a href="service.html">Serviços</a></li><!--
+<li><a href="blog.html">blog<i class="icofont icofont-simple-down"></i></a>
+<ul>
+<li><a href="blog.html">Blog</a></li>
+<li><a href="blog-details.html">Blog Details</a></li>
+</ul>
+</li>-->
+                                    <li><a href="contact.html">Agenda</a></li>
+                                    <li><a href="about.html">LOGIN</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="mobile-menu"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Hero Slide Area
+============================================ -->
+    <div id="hero-area" class="hero-slider-area">
+        <div id="hero-slider" class="nivoSlider slider-image">
+            <img src="img/slider/1.jpg" alt="main slider" title="#htmlcaption1"/>
+            <img src="img/slider/1.jpg" alt="main slider" title="#htmlcaption2"/>
+            <img src="img/slider/1.jpg" alt="main slider" title="#htmlcaption3"/>
+        </div>
+
+
+        <div id="htmlcaption1" class="nivo-html-caption">			
+            <div class="slide-table container">
+                <div class="table-cell">
+                    <div class="hero-slide-content text-center">
+                        <h3 class="wow fadeInUp" data-wow-duration=".9s" data-wow-delay="0.8s">Bem vindo a SmartAuto</h3>
+                        <h1 class="wow fadeInUp" data-wow-duration=".9s" data-wow-delay="1.2s">SMART <span>AUTO</span></h1>
+                        <p class="wow fadeInUp" data-wow-duration=".9s" data-wow-delay="1.6s">Lorem ipsum dolor sit amet, consectetur adipiscing elit, minim veniamsed sed do <br />eiusmod tempor maksu rez ut labore  magna do eiusmod</p>
+                        <div class="button-group">
+                            <a href="#" class="btn transparent wow fadeInLeft" data-wow-duration=".9s" data-wow-delay="2s">book lesson</a>
+                            <a href="#" class="btn color wow fadeInRight" data-wow-duration=".9s" data-wow-delay="2s">learn more</a>
+                        </div>
+                    </div>
+                </div>										
+            </div>
+        </div>
+    </div>
